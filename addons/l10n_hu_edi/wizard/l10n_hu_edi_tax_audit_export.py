48d9740982614573a9d75a62d08782742d682620
Antoine Dupuis (andu) <andu@odoo.com>
2024-04-12 14:49:24 +0200

    [FIX] l10n_hu_edi: Tax Audit export selection by invoice name

--- addons/l10n_hu_edi/wizard/l10n_hu_edi_tax_audit_export.py
+++ addons/l10n_hu_edi/wizard/l10n_hu_edi_tax_audit_export.py
@@ -13,11 +13,25 @@ class L10nHuEdiTaxAuditExport(models.TransientModel):
     _name = 'l10n_hu_edi.tax_audit_export'
     _description = 'Tax audit export - Adóhatósági Ellenőrzési Adatszolgáltatás'
 
+    selection_mode = fields.Selection(
+        string='Selection mode',
+        selection=[
+            ('date', 'By date'),
+            ('name', 'By serial number'),
+        ],
+        default='date',
+    )
     date_from = fields.Date(
         string='Date From'
     )
     date_to = fields.Date(
         string='Date To'
+    )
+    name_from = fields.Char(
+        string='Name From'
+    )
+    name_to = fields.Char(
+        string='Name To'
     )
     filename = fields.Char(
         string='File name',
@@ -28,22 +42,31 @@ class L10nHuEdiTaxAuditExport(models.TransientModel):
         readonly=True
     )
 
-    @api.depends('date_from', 'date_to')
+    @api.depends('selection_mode', 'date_from', 'date_to', 'name_from', 'name_to')
     def _compute_filename(self):
-        date_from = self.date_from
-        if not date_from:
-            first_invoice = self.env['account.move'].search(
-                [
-                    ('move_type', 'in', ('out_invoice', 'out_refund')),
-                    ('state', '=', 'posted'),
-                    ('country_code', '=', 'HU'),
-                ],
-                order='date',
-                limit=1,
-            )
-            date_from = first_invoice.date
-        date_to = self.date_to or fields.Date.today()
-        self.filename = f'export_{date_from}_{date_to}.zip'
+        domain = [
+            ('move_type', 'in', ('out_invoice', 'out_refund')),
+            ('state', '=', 'posted'),
+            ('country_code', '=', 'HU'),
+        ]
+        if self.selection_mode == 'date':
+            date_from = self.date_from
+            if not date_from:
+                first_invoice = self.env['account.move'].search(domain, order='date', limit=1)
+                date_from = first_invoice.date
+            date_to = self.date_to or fields.Date.today()
+            self.filename = f'export_{date_from}_{date_to}.zip'
+
+        else:
+            name_from = self.name_from
+            if not name_from:
+                first_invoice = self.env['account.move'].search(domain, order='name', limit=1)
+                name_from = first_invoice.name
+            name_to = self.name_to
+            if not name_to:
+                last_invoice = self.env['account.move'].search(domain, order='name desc', limit=1)
+                name_to = last_invoice.name
+            self.filename = f'export_{name_from.replace("/", "")}_{name_to.replace("/", "")}.zip'
 
     def action_export(self):
         self.ensure_one()
@@ -52,10 +75,16 @@ class L10nHuEdiTaxAuditExport(models.TransientModel):
             ('state', '=', 'posted'),
             ('country_code', '=', 'HU'),
         ]
-        if self.date_from:
-            domain.append(('date', '>=', self.date_from))
-        if self.date_to:
-            domain.append(('date', '<=', self.date_to))
+        if self.selection_mode == 'date':
+            if self.date_from:
+                domain.append(('date', '>=', self.date_from))
+            if self.date_to:
+                domain.append(('date', '<=', self.date_to))
+        else:
+            if self.name_from:
+                domain.append(('name', '>=', self.name_from))
+            if self.name_to:
+                domain.append(('name', '<=', self.name_to))
 
         invoices = self.env['account.move'].search(domain)
         if not invoices:
